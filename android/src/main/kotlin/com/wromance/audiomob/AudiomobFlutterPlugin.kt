cd085cb33584978e3080b2036282c7d24dbaa04b
vlastachu <vlastachu@gmail.com>
2025-03-11 11:58:45 +0300

    Unresolved reference 'AudiomobPluginListener'.

--- android/src/main/kotlin/com/wromance/audiomob/AudiomobFlutterPlugin.kt
+++ android/src/main/kotlin/com/wromance/audiomob/AudiomobFlutterPlugin.kt
@@ -2,6 +2,7 @@ package com.wromance.audiomob
 
 import BannerViewFactory
 import android.app.Activity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,13 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import com.audiomob.sdk.AudiomobPlugin
+import com.audiomob.sdk.data.responses.AdAvailability
+import com.audiomob.sdk.data.responses.AudioAd
+import com.audiomob.sdk.enums.AdPlaybackResult
+import com.audiomob.sdk.enums.AdRequestResult
 import com.audiomob.sdk.enums.BannerSize
+import com.audiomob.sdk.enums.PauseAdEnum
+import com.audiomob.sdk.interfaces.managers.IAudiomobCallback
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -207,6 +214,71 @@ class AudiomobFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
 
 }
 
+class AudiomobPluginListener: IAudiomobCallback {
+    var eventSink: EventChannel.EventSink? = null
+    var audioAd: AudioAd? = null
+
+    override fun onAdAvailabilityRetrieved(result: AdAvailability) {
+        eventSink?.success(mapOf("type" to "onAdAvailabilityRetrieved", "result" to result.toMap()))
+        Log.d("Audiomob Demo Project", "Ads are available: " + result.toMap())
+    }
+
+    override fun onAdRequestStarted() {
+        eventSink?.success(mapOf("type" to "onAdRequestStarted"))
+        Log.d("Audiomob Demo Project", "onAdRequestStarted")
+    }
+
+    override fun onAdRequestCompleted(adRequestResult: AdRequestResult, audioAd: AudioAd?) {
+        this.audioAd = audioAd
+        eventSink?.success(
+            mapOf(
+                "type" to "onAdRequestCompleted",
+                "adRequestResult" to adRequestResult.name,
+                "audioAd" to audioAd?.toMap()
+            )
+        )
+        Log.d("Audiomob Demo Project", "onAdRequestCompleted: $adRequestResult, $audioAd")
+        if (adRequestResult != AdRequestResult.FINISHED) {
+            // The ad request was not successful.
+        }
+    }
+
+    override fun onAdPlaybackStarted(audioAd: AudioAd) {
+        this.audioAd = audioAd
+        printViewHierarchy(BannerManager.getRoot()!!)
+        eventSink?.success(mapOf("type" to "onAdPlaybackStarted", "audioAd" to audioAd.toMap()))
+        Log.d("Audiomob Demo Project", "onAdPlaybackStarted: $audioAd")
+        // Playback of the ad has begun, use the callback to mute your app's sound.
+    }
+
+    override fun onAdPlaybackCompleted(adPlaybackResult: AdPlaybackResult) {
+        this.audioAd = null
+        printViewHierarchy(BannerManager.getRoot()!!)
+        eventSink?.success(
+            mapOf(
+                "type" to "onAdPlaybackCompleted",
+                "adPlaybackResult" to adPlaybackResult.name
+            )
+        )
+        Log.d("Audiomob Demo Project", "onAdPlaybackCompleted: $adPlaybackResult")
+        // Playback has completed, use this callback to unmute your app's sound.
+        if (adPlaybackResult == AdPlaybackResult.FINISHED) {
+            // The ad completed, reward your user here.
+        }
+    }
+
+    override fun onAdPlaybackPaused(pauseReason: PauseAdEnum) {
+        eventSink?.success(mapOf("type" to "onAdPlaybackPaused", "pauseReason" to pauseReason.name))
+        Log.d("Audiomob Demo Project", "onAdPlaybackPaused: $pauseReason")
+
+    }
+
+    override fun onAdPlaybackResumed() {
+        eventSink?.success(mapOf("type" to "onAdPlaybackResumed"))
+        Log.d("Audiomob Demo Project", "onAdPlaybackResumed")
+
+    }
+}
 
 fun printViewHierarchy(view: View, indent: String = "") {
     val visibilityStatus = when (view.visibility) {
